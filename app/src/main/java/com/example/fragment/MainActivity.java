f6503243e26657300138606581a0b8a1ec1e0665
Md Mehedi Hasan <145797601+mehediinf@users.noreply.github.com>
2024-01-29 05:23:17 +0600

    Second Commit Fragment

--- app/src/main/java/com/example/fragment/MainActivity.java
+++ app/src/main/java/com/example/fragment/MainActivity.java
@@ -1,14 +1,62 @@
 package com.example.fragment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import android.app.Fragment;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
+    private ListView listView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] name = {"Mehedi","Masum","Shishir","Alif","Sumon","Reday"};
+        listView = findViewById(R.id.listView_Id);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,name);
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(this);
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        Fragment fragment;
+
+        if (position==0){
+
+            fragment = new mehediFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_Id,
+                    fragment);
+            fragmentTransaction.commit();
+
+        }
+        else if (position==1){
+
+            fragment = new masumFragment();
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.fragment_Id,fragment);
+            fragmentTransaction.commit();
+
+        }
+
+
     }
 }
